fb8bb9def150acef102021196c69f297ba7243d9
Slyzzz <75564661+Commander47@users.noreply.github.com>
2026-03-18 15:26:44 +0100

    2.2.1 Leaderboard sorting (score desc, time asc) + tests updated

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -30,11 +30,12 @@ class LeaderboardControllerTests {
 
         val res: List<GameResult> = controller.getLeaderboard()
 
+        //Reihenfolge nach Time
         verify(mockedService).getGameResults()
         assertEquals(3, res.size)
-        assertEquals(first, res[0])
-        assertEquals(second, res[1])
-        assertEquals(third, res[2])
+        assertEquals(second, res[0])
+        assertEquals(third, res[1])
+        assertEquals(first, res[2])
     }
 
     @Test
@@ -47,11 +48,12 @@ class LeaderboardControllerTests {
 
         val res: List<GameResult> = controller.getLeaderboard()
 
+        //Reihenfolge nach Time
         verify(mockedService).getGameResults()
         assertEquals(3, res.size)
-        assertEquals(first, res[0])
-        assertEquals(second, res[1])
-        assertEquals(third, res[2])
+        assertEquals(second, res[0])
+        assertEquals(third, res[1])
+        assertEquals(first, res[2])
     }
 
 }
